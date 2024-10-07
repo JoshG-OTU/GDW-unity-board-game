845aff68eb8ebc65929efe69f23759890253bc6e
GhoosttR <raviramana29@gmail.com>
2024-10-07 11:51:18 -0400

    Added QTE

---
+++ FirstYearBoardGame/Assets/Scripts/QTE.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
+
+public class QTE : MonoBehaviour
+{
+    public Dictionary<int, string> Shift;
+    public Dictionary<int, string> Space;
+    public Dictionary<int, string> Control;
+    public Dictionary<int, string> Enter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assigning buttons to the players
+        Dictionary<string, string> players = new()
+        {
+            ["Red"] = "Shift",
+            ["Purple"] = "Space",
+            ["Blue"] = "Control",
+            ["Green"] = "Enter"
+        };
+
+        Debug.Log("Player 1's button is " + players["Red"]);
+        Debug.Log("Player 2's button is " + players["Purple"]);
+        Debug.Log("Player 3's button is " + players["Blue"]);
+        Debug.Log("Player 4's button is " + players["Green"]);
+
+        Debug.Log("Game Start");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Detecting button inputs
+        if (Input.GetKeyDown(KeyCode.LeftShift));
+        {
+            Debug.Log("Shift was pressed");
+            Debug.Log("Player 1 wins");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) ;
+        {
+            Debug.Log("Space was pressed");
+            Debug.Log("Player 2 wins");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Debug.Log("Control was pressed");
+            Debug.Log("Player 3 wins");
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter)) ;
+        {
+            Debug.Log("Enter was pressed");
+            Debug.Log("Player 4 wins");
+        }
+
+        
+    }
+}
---
+++ FirstYearBoardGame/Assets/Scripts/QTEsys.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QTEsys : MonoBehaviour
+{
+    public TMP_Text DisplayBox;
+    public TMP_Text Passbox;
+    public int QTEGen;
+    public int WaitingForKey;
+    public int CorrectKey;
+    public int CountingDown;
+
+    public void Update()
+    {
+        if (WaitingForKey == 0)
+        {
+            QTEGen = Random.Range(1, 2);
+            CountingDown = 1;
+            StartCoroutine(CountDown());
+
+            if (QTEGen == 1)
+            {
+                WaitingForKey = 1;
+                DisplayBox.GetComponent<TMP_Text>().text = "<sprite index=0>";
+            }
+        }
+        if (QTEGen == 1)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetButtonDown("QTE Button Input"))
+                {
+                    CorrectKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    CorrectKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+        IEnumerator KeyPressing()
+        {
+            QTEGen = 4;
+            if (CorrectKey == 1)
+            {
+                CountingDown = 2;
+                Passbox.GetComponent<TMP_Text>().text = "PASS!!";
+                yield return new WaitForSeconds(1.5f);
+                CorrectKey = 0;
+                Passbox.GetComponent<TMP_Text>().text = "";
+                DisplayBox.GetComponent<TMP_Text>().text = "";
+                yield return new WaitForSeconds(1.5f);
+                WaitingForKey = 0;
+                CountingDown = 1;
+            }
+            if (CorrectKey == 2)
+            {
+                CountingDown = 2;
+                Passbox.GetComponent<TMP_Text>().text = "Fail!!";
+                yield return new WaitForSeconds(1.5f);
+                CorrectKey = 0;
+                Passbox.GetComponent<TMP_Text>().text = "";
+                DisplayBox.GetComponent<TMP_Text>().text = "";
+                yield return new WaitForSeconds(1.5f);
+                WaitingForKey = 0;
+                CountingDown = 1;
+            }
+        }
+        IEnumerator CountDown()
+        {
+            yield return new WaitForSeconds(3.5f);
+            if (CountingDown == 1)
+            {
+                QTEGen = 4;
+                CountingDown = 2;
+                Passbox.GetComponent<TMP_Text>().text = "Fail!!";
+                yield return new WaitForSeconds(1.5f);
+                CorrectKey = 0;
+                Passbox.GetComponent<TMP_Text>().text = "";
+                DisplayBox.GetComponent<TMP_Text>().text = "";
+                yield return new WaitForSeconds(1.5f);
+                WaitingForKey = 0;
+                CountingDown = 1;
+            }
+        }
+    }
+}
